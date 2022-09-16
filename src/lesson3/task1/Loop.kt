9b0e8f40b7f05f3b5f7570de4ed48c4850697506
Hellcake <nfssanekru@gmail.com>
2022-09-16 11:13:56 +0300

    fix

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -156,11 +156,10 @@ fun collatzSteps(x: Int): Int {
     while (num != 1) {
         if (num % 2 == 0) {
             num /= 2
-            counter++
         } else {
             num = 3 * num + 1
-            counter++
         }
+        counter++
     }
     return counter
 }
